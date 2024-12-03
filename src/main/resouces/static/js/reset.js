16fce275d499d717db037c9c1e911974b1e72d15
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2024-12-04 03:27:06 +0530

    Create reset.js

---
+++ src/main/resouces/static/js/reset.js
@@ -0,0 +1,10 @@
+
+$(document).ready(() => {
+    $("#reset").click(sendReset);
+});
+
+function sendReset() {
+    $.get("reset-all", response => {
+        showAlert(true, messages.RESET_SUCCESS);
+    });
+}
